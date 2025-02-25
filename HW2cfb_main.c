1493160962cd02fb01a124fe988f237cc3bf90c5
cbelga2 <116589663+cbelga2@users.noreply.github.com>
2025-02-25 17:29:07 -0600

    Update HW2cfb_main.c

--- HW2cfb_main.c
+++ HW2cfb_main.c
@@ -15,7 +15,6 @@
 #include "driverlib.h"
 #include "device.h"
 #include "F28379dSerial.h"
-#include "LEDPatterns.h"
 #include "song.h"
 #include "dsp.h"
 #include "fpu32/fpu_rfft.h"
@@ -41,9 +40,13 @@ extern uint32_t numRXA;
 uint16_t UARTPrint = 0;
 uint16_t LEDdisplaynum = 0;
 uint16_t ADCINA4_raw = 0;
-float ADCINA4_scaled = 0;
-uint16_t ADCINA4_result = 0;
+float ADCINA4_result = 0;
 uint32_t ADCA_interruptcount = 0;
+uint16_t xaxis = 0;
+uint16_t yaxis = 0;
+
+float x_voltage = 0;
+float y_voltage = 0;
 
 // Function Variables
 int16_t updown =1; // For cpu timer 2 LED dimming function, when updown = 1, counts up, when updown = 0 count down - cfb
@@ -57,93 +60,93 @@ void main(void)
     InitSysCtrl();
 
     InitGpio();
-	
-	// Blue LED on LaunchPad
+
+    // Blue LED on LaunchPad
     GPIO_SetupPinMux(31, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(31, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPASET.bit.GPIO31 = 1;
 
-	// Red LED on LaunchPad
+    // Red LED on LaunchPad
     GPIO_SetupPinMux(34, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(34, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPBSET.bit.GPIO34 = 1;
 
-	// LED1 and PWM Pin
+    // LED1 and PWM Pin
     GPIO_SetupPinMux(22, GPIO_MUX_CPU1, 5); // Sets GPIO22 to EPwm12A
     GPIO_SetupPinOptions(22, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPACLEAR.bit.GPIO22 = 1;
-	
-	// LED2
+
+    // LED2
     GPIO_SetupPinMux(94, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(94, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPCCLEAR.bit.GPIO94 = 1;
 
-	// LED3
+    // LED3
     GPIO_SetupPinMux(95, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(95, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPCCLEAR.bit.GPIO95 = 1;
 
-	// LED4
+    // LED4
     GPIO_SetupPinMux(97, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(97, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPDCLEAR.bit.GPIO97 = 1;
 
-	// LED5
+    // LED5
     GPIO_SetupPinMux(111, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(111, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPDCLEAR.bit.GPIO111 = 1;
 
-	// LED6
+    // LED6
     GPIO_SetupPinMux(130, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(130, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPECLEAR.bit.GPIO130 = 1;
 
-	// LED7	
+    // LED7
     GPIO_SetupPinMux(131, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(131, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPECLEAR.bit.GPIO131 = 1;
 
-	// LED8
+    // LED8
     GPIO_SetupPinMux(25, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(25, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPACLEAR.bit.GPIO25 = 1;
 
-	// LED9
+    // LED9
     GPIO_SetupPinMux(26, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(26, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPACLEAR.bit.GPIO26 = 1;
 
-	// LED10
+    // LED10
     GPIO_SetupPinMux(27, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(27, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPACLEAR.bit.GPIO27 = 1;
 
-	// LED11	
+    // LED11
     GPIO_SetupPinMux(60, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(60, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPBCLEAR.bit.GPIO60 = 1;
 
-	// LED12	
+    // LED12
     GPIO_SetupPinMux(61, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(61, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPBCLEAR.bit.GPIO61 = 1;
 
-	// LED13
+    // LED13
     GPIO_SetupPinMux(157, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(157, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPECLEAR.bit.GPIO157 = 1;
 
-	// LED14
+    // LED14
     GPIO_SetupPinMux(158, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(158, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPECLEAR.bit.GPIO158 = 1;
-	
-	// LED15
+
+    // LED15
     GPIO_SetupPinMux(159, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(159, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPECLEAR.bit.GPIO159 = 1;
 
-	// LED16
+    // LED16
     GPIO_SetupPinMux(160, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(160, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPFCLEAR.bit.GPIO160 = 1;
@@ -158,7 +161,7 @@ void main(void)
     GPIO_SetupPinOptions(1, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPASET.bit.GPIO1 = 1;
 
-	//SPIRAM  CS  Chip Select
+    //SPIRAM  CS  Chip Select
     GPIO_SetupPinMux(19, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(19, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPASET.bit.GPIO19 = 1;
@@ -177,17 +180,17 @@ void main(void)
     GPIO_SetupPinMux(9, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(9, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPASET.bit.GPIO9 = 1;
-	
+
     //MPU9250  CS  Chip Select
     GPIO_SetupPinMux(66, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(66, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPCSET.bit.GPIO66 = 1;
-	
-	//WIZNET  CS  Chip Select
+
+    //WIZNET  CS  Chip Select
     GPIO_SetupPinMux(125, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(125, GPIO_OUTPUT, GPIO_PUSHPULL);
     GpioDataRegs.GPDSET.bit.GPIO125 = 1;
-	
+
     //PushButton 1
     GPIO_SetupPinMux(4, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(4, GPIO_INPUT, GPIO_PULLUP);
@@ -203,8 +206,8 @@ void main(void)
     //PushButton 4
     GPIO_SetupPinMux(7, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(7, GPIO_INPUT, GPIO_PULLUP);
-	
-	//Joy Stick Pushbutton
+
+    //Joy Stick Pushbutton
     GPIO_SetupPinMux(8, GPIO_MUX_CPU1, 0);
     GPIO_SetupPinOptions(8, GPIO_INPUT, GPIO_PULLUP);
 
@@ -273,7 +276,7 @@ void main(void)
     EALLOW;
     EPwm4Regs.ETSEL.bit.SOCAEN = 0; // Disable SOC on A group
     EPwm4Regs.TBCTL.bit.CTRMODE = 3; // freeze counter
-    EPwm4Regs.ETSEL.bit.SOCASEL = 010; // Select Event when counter equal to PRD
+    EPwm4Regs.ETSEL.bit.SOCASEL = 2; // Select Event when counter equal to PRD
     EPwm4Regs.ETPS.bit.SOCAPRD = 01; // Generate pulse on 1st event (“pulse” is the same as “trigger”)
     EPwm4Regs.TBCTR = 0x0; // Clear counter
     EPwm4Regs.TBPHS.bit.TBPHS = 0x0000; // Phase is 0
@@ -305,13 +308,13 @@ void main(void)
     AdcaRegs.ADCSOC0CTL.bit.CHSEL = 0x4;//SOC0 will convert Channel you choose Does not have to be A0
     AdcaRegs.ADCSOC0CTL.bit.ACQPS = 99; //sample window is acqps + 1 SYSCLK cycles = 500ns
     AdcaRegs.ADCSOC0CTL.bit.TRIGSEL = 0x0B;// EPWM4 ADCSOCA
-    //AdcaRegs.ADCSOC1CTL.bit.CHSEL = 0x1;//SOC1 will conv Channel you choose Does not have to be A1
-    //AdcaRegs.ADCSOC1CTL.bit.ACQPS = 99; //sample window is acqps + 1 SYSCLK cycles = 500ns
-    //AdcaRegs.ADCSOC1CTL.bit.TRIGSEL = 0x0B;// EPWM4 ADCSOCA
-    //AdcaRegs.ADCSOC2CTL.bit.CHSEL = 0x2;//SOC2 will conv Channel you choose Does not have to be A2
-    //AdcaRegs.ADCSOC2CTL.bit.ACQPS = 99; //sample window is acqps + 1 SYSCLK cycles = 500ns
-    //AdcaRegs.ADCSOC2CTL.bit.TRIGSEL = 0x0B;// EPWM4 ADCSOCA
-    AdcaRegs.ADCINTSEL1N2.bit.INT1SEL=0x1;//set to last or only SOC that is converted and it will set INT1 flag ADCA1. Only looking at SOC0, the only soc setup is 0.
+    AdcaRegs.ADCSOC1CTL.bit.CHSEL = 0x2;//SOC1 will conv Channel you choose Does not have to be A1
+    AdcaRegs.ADCSOC1CTL.bit.ACQPS = 99; //sample window is acqps + 1 SYSCLK cycles = 500ns
+    AdcaRegs.ADCSOC1CTL.bit.TRIGSEL = 0x0B;// EPWM4 ADCSOCA
+    AdcaRegs.ADCSOC2CTL.bit.CHSEL = 0x3;//SOC2 will conv Channel you choose Does not have to be A2
+    AdcaRegs.ADCSOC2CTL.bit.ACQPS = 99; //sample window is acqps + 1 SYSCLK cycles = 500ns
+    AdcaRegs.ADCSOC2CTL.bit.TRIGSEL = 0x0B;// EPWM4 ADCSOCA
+    AdcaRegs.ADCINTSEL1N2.bit.INT1SEL=0x2;//set to last or only SOC that is converted and it will set INT1 flag ADCA1. Only looking at SOC0, the only soc setup is 0.
     AdcaRegs.ADCINTSEL1N2.bit.INT1E = 1;   //enable INT1 flag
     AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1; //make sure INT1 flag is cleared
     EDIS;
@@ -345,11 +348,11 @@ void main(void)
 
     // Enable TINT0 in the PIE: Group 1 interrupt 7
     PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
-	// Enable SWI in the PIE: Group 12 interrupt 9
+    // Enable SWI in the PIE: Group 12 interrupt 9
     PieCtrlRegs.PIEIER12.bit.INTx9 = 1;
     // Enagle PIE interrupt 1.1, I'm a little confused of the notation of this.FPI
     PieCtrlRegs.PIEIER1.bit.INTx1=1;
-	
+
     // Enable global Interrupts and higher priority real-time debug events
     EINT;  // Enable Global interrupt INTM
     ERTM;  // Enable Global realtime interrupt DBGM
@@ -359,9 +362,10 @@ void main(void)
     while(1)
     {
         if (UARTPrint == 1 ) {
-				//serial_printf(&SerialA,"Num Timer2:%ld Num SerialRX: %ld\r\n",CpuTimer2.InterruptCount,numRXA);
+                //serial_printf(&SerialA,"Num Timer2:%ld Num SerialRX: %ld\r\n",CpuTimer2.InterruptCount,numRXA);
             // When UARTPRINT ==1, print ADCINA4_result
-            serial_printf(&SerialA,"ADCA1 V: %d \r\n", ADCINA4_result);
+            // serial_printf(&SerialA,"ADCA1 V: %f \r\n", ADCINA4_result);
+            serial_printf(&SerialA,"Vx: %f, Vy: %f \r\n", x_voltage, y_voltage);
             UARTPrint = 0;
         }
     }
@@ -373,17 +377,62 @@ __interrupt void ADCA_ISR (void) {
     PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
     // read ADCINA4 into ADCINA4_raw
     ADCINA4_raw = AdcaResultRegs.ADCRESULT0;
+    xaxis = AdcaResultRegs.ADCRESULT2;
+    yaxis = AdcaResultRegs.ADCRESULT1;
+
+
     // Convert ADCINA4 to voltage and store into ADCINA4_ result;
     ADCINA4_result = ADCINA4_raw * (3.0/4096.0);
+    x_voltage = xaxis * (3/4096.0);
+    y_voltage = yaxis * (3/4096.0);
     // Print ADCINA4’s voltage value to TeraTerm every 100ms by setting UARTPrint to one every 100th  time in this function.
     if ((ADCA_interruptcount % 100) == 0) {
             UARTPrint = 1;
         }
 
-    EPwm12Regs.CMPA.bit.CMPA = ADCINA4_result/5000;
+    // Turn voltage into descretized value for CMPA
+    EPwm12Regs.CMPA.bit.CMPA = ADCINA4_result * (5000/3);
+    if (x_voltage <= 1.7 && x_voltage >= 1.5 && y_voltage >= 1.4 && y_voltage <= 1.6) {
+        GpioDataRegs.GPASET.bit.GPIO25 = 1;  // 8 on
+        GpioDataRegs.GPECLEAR.bit.GPIO130 = 1; // 6 off
+        GpioDataRegs.GPACLEAR.bit.GPIO27 = 1; // 10 off
+        GpioDataRegs.GPECLEAR.bit.GPIO157 = 1; // 13 off
+        GpioDataRegs.GPCCLEAR.bit.GPIO95 = 1; // 3 off
+    }
+
+    if (x_voltage >  1.7) {
+        GpioDataRegs.GPACLEAR.bit.GPIO25 = 1;  // 8 off
+        GpioDataRegs.GPESET.bit.GPIO130 = 1; // 6 on
+        GpioDataRegs.GPACLEAR.bit.GPIO27 = 1; // 10 off
+        GpioDataRegs.GPECLEAR.bit.GPIO157 = 1; // 13 off
+        GpioDataRegs.GPCCLEAR.bit.GPIO95 = 1; // 3 off
+    }
+    if (x_voltage < 1.5) {
+        GpioDataRegs.GPACLEAR.bit.GPIO25 = 1;  // 8 off
+        GpioDataRegs.GPECLEAR.bit.GPIO130 = 1; // 6 off
+        GpioDataRegs.GPASET.bit.GPIO27 = 1; // 10 on
+        GpioDataRegs.GPECLEAR.bit.GPIO157 = 1; // 13 off
+        GpioDataRegs.GPCCLEAR.bit.GPIO95 = 1; // 3 off
+    }
+    if (y_voltage > 1.6) {
+        GpioDataRegs.GPACLEAR.bit.GPIO25 = 1;  // 8 off
+        GpioDataRegs.GPECLEAR.bit.GPIO130 = 1; // 6 off
+        GpioDataRegs.GPACLEAR.bit.GPIO27 = 1; // 10 off
+        GpioDataRegs.GPECLEAR.bit.GPIO157 = 1; // 13 off
+        GpioDataRegs.GPCSET.bit.GPIO95 = 1; // 3 on
+    }
+    if (y_voltage < 1.4) {
+        GpioDataRegs.GPACLEAR.bit.GPIO25 = 1;  // 8 off
+        GpioDataRegs.GPECLEAR.bit.GPIO130 = 1; // 6 off
+        GpioDataRegs.GPACLEAR.bit.GPIO27 = 1; // 10 off
+        GpioDataRegs.GPESET.bit.GPIO157 = 1; // 13 on
+        GpioDataRegs.GPCCLEAR.bit.GPIO95 = 1; // 3 off
+    }
+
     ADCA_interruptcount++;
     AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1;  //clear interrupt flag
     PieCtrlRegs.PIEACK.all = PIEACK_GROUP1; // ready for more interrupts
+
 }
 
 
@@ -391,18 +440,18 @@ __interrupt void ADCA_ISR (void) {
 __interrupt void SWI_isr(void) {
 
     // These three lines of code allow SWI_isr, to be interrupted by other interrupt functions
-	// making it lower priority than all other Hardware interrupts.  
-	PieCtrlRegs.PIEACK.all = PIEACK_GROUP12;
+    // making it lower priority than all other Hardware interrupts.
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP12;
     asm("       NOP");                    // Wait one cycle
     EINT;                                 // Clear INTM to enable interrupts
-	
-	
-	
+
+
+
     // Insert SWI ISR Code here.......
-	
-	
+
+
     numSWIcalls++;
-    
+
     DINT;
 
 }
@@ -429,7 +478,7 @@ __interrupt void cpu_timer0_isr(void)
                  // }
     }
 
-	
+
     // Acknowledge this interrupt to receive more interrupts from group 1
     PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
 }
@@ -437,8 +486,8 @@ __interrupt void cpu_timer0_isr(void)
 // cpu_timer1_isr - CPU Timer1 ISR
 __interrupt void cpu_timer1_isr(void)
 {
-	
-	
+
+
     CpuTimer1.InterruptCount++;
 }
 
@@ -470,7 +519,7 @@ __interrupt void cpu_timer2_isr(void)
     */
     CpuTimer2.InterruptCount++;
 
-	if ((CpuTimer2.InterruptCount % 50) == 0) {
-		UARTPrint = 1;
-	}
+    if ((CpuTimer2.InterruptCount % 50) == 0) {
+        UARTPrint = 1;
+    }
 }
